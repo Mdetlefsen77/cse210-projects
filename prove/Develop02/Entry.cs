f1a358d4fdfebb22b9770ed432f00717c925fd72
Mdetlefsen77 <martin.detle@gmail.com>
2024-03-09 16:31:29 -0300

    PREP2 and PROVE W02 is done

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Entry
+{
+    public string Date { get; set; }
+    public string Prompt { get; set; }
+    public string Text { get; set; }
+
+    public Entry(string date, string prompt, string text)
+    {
+        Date = date;
+        Prompt = prompt;
+        Text = text;
+    }
+}
+
